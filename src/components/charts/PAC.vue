7ab5f50451cf1cab67817e85f5cf58352c02f3bc
alvaropfn <alvaropfn@gmail.com>
2024-11-25 23:05:37 -0300

    vue-apexchart

--- src/components/charts/PAC.vue
+++ src/components/charts/PAC.vue
@@ -1,37 +1,37 @@
 <script setup>
-import { onMounted } from 'vue'
-import ApexCharts from 'apexcharts'
-var options = {
+import { onMounted, onUpdated, reactive } from 'vue'
+const options = {
   chart: {
-    type: 'line'
+    id: 'vuechart-example'
   },
-  series: [{
-    name: 'sales',
-    data: [30,40,35,50,49,60,70,91,125]
-  }],
   xaxis: {
-    categories: [1991,1992,1993,1994,1995,1996,1997, 1998,1999]
+    categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998]
   }
 }
 
-var chart = new ApexCharts(document.querySelector("#chart"), options);
+const series = [{
+  name: 'series-1',
+  data: [30, 40, 45, 50, 49, 60, 70, 91]
+}]
 
-chart.render();
+const add = () => {
 
-onMounted(() => {
-  var chart = new ApexCharts(document.querySelector("#chart"), options);
-  chart.render();
-})
+  options.series[0].data.push(111)
+  options.series[1].data.push(50)
+  options.xaxis.categories.push(2000)
+}
+
+
 </script>
 
 <template>
-  <h1>PAC</h1>
-  <div id="chart"></div>
+  <div>
+    <h1>PAC</h1>
+    <apexchart width="500" type="bar" :options="options" :series="series"></apexchart>
+    <button @click="add">add</button>
+  </div>
 </template>
 
 <style scoped>
-#chart {
-  width: 100%;
-  height: 350px;
-}
+
 </style>
